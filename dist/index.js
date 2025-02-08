f34b49e10d0d2b2744849b3167aeade30245df8d
tom <tom@monck.info>
2025-02-08 07:55:11 -0500

    Adjusting log a little

--- dist/index.js
+++ dist/index.js
@@ -350,7 +350,7 @@ const parseModules = (file, threshold, changedFilesAndLineNumbers) => {
             const coverableLines = lines.map(line => Number(line['$'].number));
             if (file) {
                 console.log(file.name);
-                console.log(JSON.stringify(changedFilesAndLineNumbers));
+                console.log(JSON.stringify(changedFilesAndLineNumbers[0].name));
                 const changedFile = changedFilesAndLineNumbers.find(f => f.name === file.name);
                 const changedLines = (changedFile === null || changedFile === void 0 ? void 0 : changedFile.lineNumbers.filter(ln => coverableLines.includes(Number(ln)))) || [];
                 file.linesTotal += Number(lines.length);
